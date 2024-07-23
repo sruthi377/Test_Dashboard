794a3b3e7e33cee75134d5be31140422a9b1696c
sruthi377 <sruthi@cogniphi.com>
2024-07-23 15:54:34 +0530

    changes in appjs file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <div className='Header'>
-<h1>KFC JAPN DASHBOARD</h1>
+        <h1>KFC JAPN DASHBOARD</h1>
+        <h2>Drivethrough dashboard</h2>
       </div>
     </div>
   );
