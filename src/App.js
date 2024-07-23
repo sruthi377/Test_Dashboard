429c9332279bf57792050886b0203451d06f4e9c
sruthi377 <sruthi@cogniphi.com>
2024-07-23 15:52:54 +0530

    commit all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Header'>
+<h1>KFC JAPN DASHBOARD</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
